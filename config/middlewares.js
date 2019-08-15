f7fc81983364186c9d4233c3f15a0ec38185026c
Allan Wanderley Alves <awallan259@gmail.com>
2019-08-15 01:32:49 -0300

    Adicionado allow origins no cors - produção

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,33 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+/* Lista de origins permitidos para consumo da api */
+/*  
+    Adicione somente mais origins em casos de novas
+    aplicações que irão consumir a mesma
+*/
+const whiteList = ['https://painel.codermind.com.br']
+
+/*      
+    Configurações de acesso aos middlewares via cors
+    Saiba mais em: https://www.npmjs.com/package/cors
+
+    Ao omitir este valor no parametro da função cors será permitido
+    Acesso de qualquer origin
+*/
+
+const options = {
+    origin: (origin, callback) => {
+        if(whiteList.indexOf(origin) !== -1){
+            callback(null, true)
+        }else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+
 module.exports = app => {
     app.use(bodyParser.json({limit: '10mb'}))
-    app.use(cors())
+    app.use(cors(options))
 }
