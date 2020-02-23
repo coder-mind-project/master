713c0309e4f99763dfc09df0110a1aca7382a7f3
Allan Wanderley <awallan259@gmail.com>
2020-02-23 06:18:53 -0300

    Eslint rules applied

--- config/database/mysqlDB.js
+++ config/database/mysqlDB.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const config = require('../../knexfile.js')
 
 module.exports = app => {
-    const {local, development, production} = config
-    const connection = knex(development)
-    app.knex = connection
+  const { local, development, production } = config
+  const connection = knex(development)
+  app.knex = connection
 }
