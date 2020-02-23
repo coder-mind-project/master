ffc509dfa319cb98590dc2d49b996fb57a5f9b08
Allan Wanderley <awallan259@gmail.com>
2020-02-23 06:16:44 -0300

    refactored categories module

--- api/categories/categories.js
+++ api/categories/categories.js
@@ -1,184 +1,265 @@
+/**
+ * @function
+ * @module Categories
+ * @description Provide some middlewares functions.
+ * @param {Object} app - A app Object provided by consign.
+ * @returns {Object} Containing some middleware functions.
+ */
 module.exports = app => {
+  const { exists, validateLength } = app.config.validation
 
-    // Validação de dados
-    const { exists, validateLength } = app.config.validation
-    
-    // Mongoose model para categorias
-    const { Category } = app.config.database.schemas.mongoose
-    
-    // Responsável por gerar Mensagens de erro Personalizadas
-    const { errorCategory } = app.config.api.httpResponses
+  const { Category } = app.config.database.schemas.mongoose
 
-    const save = async (req, res) => {
-        /* Responsável por persistir categorias */
+  const { errorCategory } = app.config.api.httpResponses
 
-        const category = {...req.body}
+  /**
+   * @function
+   * @description Save a category
+   * @param {Object} req - Request object provided by Express.js
+   * @param {Object} res - Response object provided by Express.js
+   *
+   * @returns {Object} A Category Object representation
+   *
+   * @middlewareParams {String} id - Category identifier
+   */
+  const save = async (req, res) => {
+    const category = { ...req.body }
+    const _id = req.params.id || null
 
+    try {
+      /* Data validations */
+      exists(category.name, {
+        name: 'name',
+        description: 'Nome da categoria não informado'
+      })
 
-        try {
-            exists(category.name, 'Categoria não informada')
-            exists(category.theme, 'Tema não informado')
-            validateLength(category.name, 30, 'bigger')
-            validateLength(category.alias, 30, 'bigger')
-            validateLength(category.description, 100, 'bigger')
-            
-            category.state = 'active'
-        } catch (msg) {
-            return res.status(400).send(msg)
-        }
+      exists(category.theme, {
+        name: 'theme',
+        description: 'Tema não informado'
+      })
 
-        try {
-            if(!category._id){
-                delete category._id 
+      exists(category.theme._id, {
+        name: 'theme',
+        description: 'Tema não informado'
+      })
 
-                const newCategory = new Category(category)
+      validateLength(category.name, 30, 'bigger', {
+        name: 'name',
+        description: 'Categoria muito grande, máximo permitido são 30 caracteres'
+      })
 
-                await newCategory.save().then( (response) => res.status(201).send(response)).catch(error => {
-                    if(error.code === 11000) throw 'Ja existe categoria com este nome'
-                    else throw 'Ocorreu um erro desconhecido, se persistir reporte'
-                })
-            }else{
-                await Category.updateOne({_id: category._id}, category).then(() => res.status(204).send()).catch(error => {
-                    if(error.code === 11000) throw 'Ja existe categoria com este nome'
-                    else throw 'Ocorreu um erro desconhecido, se persistir reporte'
-                })
-            }
-        } catch (error) {
-            error = await errorCategory(error)
-            return res.status(error.code).send(error.msg)
-        }
-    }
+      validateLength(category.alias, 30, 'bigger', {
+        name: 'alias',
+        description: 'Apelido muito grande, máximo permitido são 30 caracteres'
+      })
 
-    const get = async (req, res) => {
-        /*  Responsável por obter as categorias por filtros de 
-            palavras chave. Ocorrendo a possibilidade de limitar 
-            por páginação e também obtendo a quantidade total de registros
-            por filtragem
-         */
-        
-        try {
-            var limit = parseInt(req.query.limit) || 10
-            const query = req.query.query || ''
-            const page = req.query.page || 1
+      validateLength(category.description, 100, 'bigger', {
+        name: 'name',
+        description: 'Descrição muito grande, máximo permitido são de 100 caracteres'
+      })
+      /* End data validations */
 
-            if(limit > 100) limit = 10
-
-            let count = await Category.aggregate([
-                {$match : 
-                    {$and: [
-                            {$or : [
-                                {name: {$regex: `${query}` , $options: 'i'}},
-                                {alias: {$regex: `${query}` , $options: 'i'}},
-                            ]
-                            },
-                            {
-                                state: 'active'
-                            }
-                        ]
-                    }
-                }
-            ]).count('id')
-
-            count = count.length > 0 ? count.reduce(item => item).id : 0
-
-            Category.aggregate([
-                {$match : 
-                    {$and: [
-                            {$or : [
-                                {name: {$regex: `${query}` , $options: 'i'}},
-                                {alias: {$regex: `${query}` , $options: 'i'}},
-                            ]
-                            },
-                            {
-                                state: 'active'
-                            }
-                        ]
-                    }
-                }
-            ]).skip(page * limit - limit).limit(limit).then(categories => res.json({categories, count, limit}))
-        } catch (error) {
-            return res.status(500).send('Ops, ocorreu um erro ao recuperar as informações. Tente atualizar a página')
-        }
-    }
-
-    const remove = async (req, res) => {
-        /* Responsável por remover a categoria */
-
-        try {
-            const _id = req.params.id
-
-            if(!_id) throw 'Categoria não encontrada'
-
-            const state = {
-                state: 'removed'
-            }
-
-            Category.updateOne({_id}, state).then(() => res.status(204).send())
-        } catch (error) {
-            error = await errorCategory(error)
-            return res.status(error.code).send(error.msg)
-        }
-    }
-
-    const getOne = (req, res) => {
-        /* Responsável por obter a categoria pelo ID */
-
-        const _id = req.params.id
-        Category.findOne({_id}).then(category => res.json(category)).catch( async (error) => {
-            error = await errorCategory(error)
-            return res.status(error.code).send(error.msg)
+      if (!_id) {
+        // Create a new category
+        const result = await new Category(category).save()
+        if (result._id) return res.status(201).send(result)
+        throw result
+      } else {
+        // Update a category
+        await Category.updateOne({ _id }, category).then(() => {
+          Category.findOne({ _id }).then(category => {
+            res.json(category)
+          })
         })
+      }
+    } catch (error) {
+      const stack = await errorCategory(error)
+      return res.status(stack.code).send(stack)
     }
-    
-    const active = async (req, res) => {
-        /* Responsável restaurar uma categoria excluída */
-        // Não implementado
+  }
 
-        try {
-            const _id = req.params.id
-            
-            if(!_id) throw 'Categoria não encontrada'
-            
-            const state = {
+  /**
+   * @function
+   * @description Gets some categories
+   * @param {Object} req - Request object provided by Express.js
+   * @param {Object} res - Response object provided by Express.js
+   *
+   * @middlewareParams {Number} limit - Limit categories per page
+   * @middlewareParams {String} query - Keyword to search for categories
+   * @middlewareParams {Number} page - Current page
+   *
+   * @returns {Object} A object containing count, limit and Categories Objects representations
+   */
+  const get = async (req, res) => {
+    try {
+      let limit = parseInt(req.query.limit) || 10
+      const query = req.query.query || ''
+      const page = parseInt(req.query.page) || 1
+
+      if (limit > 100) limit = 10
+
+      let count = await Category.aggregate([
+        {
+          $match: {
+            $and: [
+              {
+                $or: [
+                  { name: { $regex: `${query}`, $options: 'i' } },
+                  { alias: { $regex: `${query}`, $options: 'i' } }
+                ]
+              },
+              {
                 state: 'active'
-            }
-            
-            Category.updateOne({_id}, state).then(() => res.status(204).send())
-        } catch (error) {
-            const _error = await errorCategory(error)
-            return res.status(_error).send(error)
+              }
+            ]
+          }
         }
+      ]).count('id')
+
+      count = count.length > 0 ? count.reduce(item => item).id : 0
+
+      Category.aggregate([
+        {
+          $match: {
+            $and: [
+              {
+                $or: [
+                  { name: { $regex: `${query}`, $options: 'i' } },
+                  { alias: { $regex: `${query}`, $options: 'i' } }
+                ]
+              },
+              {
+                state: 'active'
+              }
+            ]
+          }
+        }
+      ])
+        .skip(page * limit - limit)
+        .limit(limit)
+        .then(categories => res.json({ categories, count, limit }))
+    } catch (error) {
+      const stack = await errorCategory(error)
+      return res.status(stack.code).send(stack)
     }
+  }
 
-    const getByTheme = (req, res) => {
-        /* Obtém todas as categorias de um determinado tema */
+  /**
+   * @function
+   * @description Remove a category
+   * @param {Object} req - Request object provided by Express.js
+   * @param {Object} res - Response object provided by Express.js
+   *
+   * @middlewareParams {String} id - Category identifier
+   */
+  const remove = async (req, res) => {
+    try {
+      const _id = req.params.id
 
-        const _id = req.params.id
-
-        try {
-            if(!_id) throw 'Cadastro não encontrado'
-        } catch (error) {
-            return res.status(400).send(error)
+      if (!_id) {
+        throw {
+          name: 'remove',
+          description: 'Categoria não encontrada'
         }
+      }
 
-        try {
-            Category.aggregate([
-                {$match : 
-                    {$and: [
-                            {
-                                'theme._id': _id 
-                            },
-                            {
-                                state: 'active'
-                            }
-                        ]
-                    }
-                }
-            ]).then(categories => res.json(categories))
-        } catch (error) {
-            return res.status(500).send(error)
-        }
+      const state = {
+        state: 'removed'
+      }
+
+      Category.updateOne({ _id }, state).then(() => res.status(204).send())
+    } catch (error) {
+      const stack = await errorCategory(error)
+      return res.status(stack.code).send(stack)
     }
+  }
 
-    return {save, get, getOne, remove, active, getByTheme}
+  /**
+   * @function
+   * @description Get a category by Id
+   * @param {Object} req - Request object provided by Express.js
+   * @param {Object} res - Response object provided by Express.js
+   *
+   * @middlewareParams {String} id - Category identifier
+   *
+   * @returns {Object} A Category Object representation
+   */
+  const getOne = (req, res) => {
+    const _id = req.params.id
+    Category.findOne({ _id })
+      .then(category => res.json(category))
+      .catch(async error => {
+        const stack = await errorCategory(error)
+        return res.status(stack.code).send(stack)
+      })
+  }
+
+  /**
+   * @function
+   * @description Gets categories by theme
+   * @param {Object} req - Request object provided by Express.js
+   * @param {Object} res - Response object provided by Express.js
+   *
+   * @middlewareParams {String} id - Category identifier
+   *
+   * @returns {Array<Category>} An Array containing categories
+   */
+  const getByTheme = async (req, res) => {
+    const themeId = req.params.id
+
+    try {
+      if (!themeId) {
+        throw {
+          name: 'themeId',
+          description: 'Identificador do tema não encontrado'
+        }
+      }
+
+      Category.aggregate([
+        {
+          $match: {
+            $and: [
+              {
+                'theme._id': themeId
+              },
+              {
+                state: 'active'
+              }
+            ]
+          }
+        }
+      ]).then(categories => res.json(categories))
+    } catch (error) {
+      const stack = await errorCategory(error)
+      return res.status(stack.code).send(stack)
+    }
+  }
+
+  /**
+   * @deprecated since version 2.0
+   * @function
+   * @description Sets 'active' state in category
+   * @param {Object} req - Request object provided by Express.js
+   * @param {Object} res - Response object provided by Express.js
+   *
+   * @middlewareParams {String} id - Category identifier
+   */
+  const active = async (req, res) => {
+    try {
+      const _id = req.params.id
+
+      if (!_id) throw 'Categoria não encontrada'
+
+      const state = {
+        state: 'active'
+      }
+
+      Category.updateOne({ _id }, state).then(() => res.status(204).send())
+    } catch (error) {
+      const stack = await errorCategory(error)
+      return res.status(stack.code).send(stack)
+    }
+  }
+
+  return { save, get, getOne, remove, active, getByTheme }
 }
