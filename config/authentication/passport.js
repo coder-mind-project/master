b3991ed1aeaf385b7e2dd9e5a6e66a572dd326a0
Allan Wanderley <awallan259@gmail.com>
2020-02-24 11:36:53 -0300

    Added doc spec

--- config/authentication/passport.js
+++ config/authentication/passport.js
@@ -1,33 +1,47 @@
-const { authSecret } = require('../../.env')
+const { SECRET_AUTH_PACKAGE } = require('../../.env')
 const passport = require('passport')
 const passportJwt = require('passport-jwt')
 const { Strategy, ExtractJwt } = passportJwt
 
+/**
+ *  @function
+ *  @module Passport
+ *  @description Generate a user passport for access middlewares that need authentication.
+ *  @param {Object} app - A app Object provided by consign.
+ *  @returns {Object} Informations included a user extracted from jwt token.
+ */
 module.exports = app => {
-    
-    const { User } = app.config.database.schemas.mongoose
+  const { User } = app.config.database.schemas.mongoose
 
-    const params = {
-        secretOrKey: authSecret,
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+  const { secret } = SECRET_AUTH_PACKAGE
+
+  const params = {
+    secretOrKey: secret,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+  }
+
+  const strategy = new Strategy(params, async (payload, done) => {
+    const _id = payload.user._id
+    const countUser = await User.countDocuments()
+    if (countUser) {
+      User.findOne({ _id, deleted: false })
+        .then(user => {
+          done(null, user ? { ...payload } : false)
+        })
+        .catch(error => done(error, false))
+    } else {
+      const user = await app.knex
+        .select()
+        .from('users')
+        .where('id', _id)
+        .first()
+      done(null, user ? { ...payload } : false)
     }
+  })
 
-    const strategy = new Strategy(params, async (payload, done) => {
-        const _id = payload.user._id
-        const countUser = await User.countDocuments()
-        if(countUser){
-            User.findOne({_id, deleted: false}).then( user => {
-                done(null, user ? {...payload} : false)
-            }).catch(error => done(error, false))
-        }else{
-            const user = await app.knex.select().from('users').where('id', _id).first()
-            done(null, user ? {...payload} : false)
-        }
-    })
+  passport.use(strategy)
 
-    passport.use(strategy)
-
-    return {
-        authenticate: () => passport.authenticate('jwt', {session: false})
-    }
+  return {
+    authenticate: () => passport.authenticate('jwt', { session: false })
+  }
 }
