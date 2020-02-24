619636db1cf64cad170af483d33caa868dc3398b
Allan Wanderley <awallan259@gmail.com>
2020-02-24 11:18:55 -0300

    Rewrite module for added doc specs and changed authSecret variable

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -1,62 +1,100 @@
-const jwt = require("jwt-simple");
-const { authSecret, issuer, panel } = require("../../.env");
-const rp = require("request-promise");
+const jwt = require('jwt-simple')
+const { SECRET_AUTH_PACKAGE, issuer } = require('../../.env')
+const rp = require('request-promise')
 
+/**
+ *  @function
+ *  @module Auth
+ *  @description Manage authentications.
+ *  @param {Object} app - A app Object provided by consign.
+ *  @returns {Object} Containing some functions for provide authentication.
+ */
 module.exports = app => {
-  // Validações de dados
-  const { exists } = app.config.validation;
+  const { exists } = app.config.validation
 
-  // Mongoose Model para usuarios
-  const { User } = app.config.database.schemas.mongoose;
+  const { User } = app.config.database.schemas.mongoose
 
-  // Configurações extras
-  const { encryptAuth } = app.config.secrets;
+  const { encryptAuth } = app.config.secrets
 
-  const { validateTokenManagement, signInError } = app.config.api.httpResponses;
+  const { authError } = app.config.api.httpResponses
 
-  const { secret_key, uri } = app.config.captcha;
+  const { secretKey, url } = app.config.captcha
 
+  /**
+   * @function
+   * @description Realize authentications.
+   * @param {Object} req - Request object provided by Express.js
+   * @param {Object} res - Response object provided by Express.js
+   *
+   * @returns {Object} A User Object representation + jwt token.
+   *
+   * @middlewareParams {Object} Localizated in Body request, must contain email, password and response attributes.
+   */
   const signIn = async (req, res) => {
-    /* Realiza a autenticação do usuário no sistema */
-
     try {
-      const request = { ...req.body };
+      const request = { ...req.body }
 
-      const url = `${uri}?secret=${secret_key}&response=${request.response}`;
+      const { secret } = SECRET_AUTH_PACKAGE
 
-      await rp({ method: "POST", uri: url, json: true }).then(response => {
-        if (!response.success) throw "Captcha inválido";
-      });
+      const uri = `${url}?secret=${secretKey}&response=${request.response}`
 
-      const countUsers = await User.countDocuments();
+      await rp({ method: 'POST', uri, json: true }).then(response => {
+        if (!response.success) {
+          throw {
+            name: 'captcha',
+            description: 'Captcha inválido'
+          }
+        }
+      })
 
-      exists(request.email, "É necessário informar um e-mail ou username");
-      exists(request.password, "É necessário informar uma senha");
+      const countUsers = await User.countDocuments()
 
+      exists(request.email, {
+        name: 'emailOrUsername',
+        description: 'É necessário informar um e-mail ou username'
+      })
+
+      exists(request.password, {
+        name: 'password',
+        description: 'É necessário informar uma senha'
+      })
+
+      // prettier-ignore
       const user = countUsers
         ? await User.findOne({ email: request.email })
         : await app.knex
-            .select()
-            .from("users")
-            .where("username", request.email)
-            .orWhere("email", request.email)
-            .first();
+          .select()
+          .from('users')
+          .where('username', request.email)
+          .orWhere('email', request.email)
+          .first()
 
-      if (!user) throw "E-mail ou senha inválidos";
-      if (user.deleted)
-        throw "Sua conta esta suspensa, em caso de reinvidicação entre em contato com o administrador do sistema";
+      if (!user) {
+        throw {
+          name: 'authentication',
+          description: 'E-mail ou senha inválidos'
+        }
+      }
 
-      const password = await encryptAuth(request.password);
+      if (user.deleted) {
+        throw {
+          name: 'authentication',
+          description:
+            'Sua conta esta suspensa, em caso de reinvidicação entre em contato com o administrador do sistema'
+        }
+      }
+
+      const password = await encryptAuth(request.password)
 
       if (user.password === password) {
         if (!user.firstLogin && !user.id) {
-          await User.updateOne({ _id: user._id }, { firstLogin: true });
+          await User.updateOne({ _id: user._id }, { firstLogin: true })
         }
 
-        user.password = null;
+        user.password = null
 
-        const now = Math.floor(Date.now() / 1000);
-        const tenDaysLater = 60 * 60 * 24 * 10;
+        const now = Math.floor(Date.now() / 1000)
+        const tenDaysLater = 60 * 60 * 24 * 10
 
         const payload = {
           iss: issuer,
@@ -70,79 +108,94 @@ module.exports = app => {
             tagAuthor: user.tagAuthor,
             platformStats: Boolean(user.platformStats)
           }
-        };
+        }
         return res.json({
-          token: jwt.encode(payload, authSecret),
+          token: jwt.encode(payload, secret),
           user
-        });
-      } else throw "E-mail ou senha inválidos";
-    } catch (error) {
-      error = await signInError(error);
-      return res.status(error.code).send(error.msg);
-    }
-  };
-
-  const validateToken = async (req, res) => {
-    /* Valida o token de acesso */
-
-    var token = null;
-    var payload = null;
-
-    try {
-      const token = { ...req.body }.token;
-
-      if (token) {
-        payload = await jwt.decode(token, authSecret);
+        })
       } else {
-        throw "Acesso não autorizado";
+        throw {
+          name: 'authentication',
+          description: 'E-mail ou senha inválidos'
+        }
       }
     } catch (error) {
-      return res.status(401).send("Acesso não autorizado");
+      const stack = await authError(error)
+      return res.status(stack.code).send(stack)
     }
+  }
 
+  /**
+   * @function
+   * @description Validate passport token.
+   * @param {Object} req - Request object provided by Express.js
+   * @param {Object} res - Response object provided by Express.js
+   *
+   * @returns {Object} A User Object representation + jwt token.
+   *
+   * @middlewareParams {Object} Localizated in Body request, must contain token attribute.
+   */
+  const validateToken = async (req, res) => {
     try {
-      const user = payload.user;
+      let token = { ...req.body }.token
+      const { secret } = SECRET_AUTH_PACKAGE
 
-      if (payload.iss !== issuer) throw "Acesso não autorizado";
+      const payload = token ? await jwt.decode(token, secret) : {}
+      const user = payload.user
 
-      const origin = isNaN(user._id);
+      if (payload.iss !== issuer) {
+        throw {
+          name: 'issuer',
+          description: 'Acesso não autorizado'
+        }
+      }
 
+      const origin = isNaN(user._id)
+
+      // prettier-ignore
       const exist = origin
         ? await User.findOne({ _id: user._id, deleted: false })
         : await app.knex
-            .select()
-            .from("users")
-            .where("id", user._id)
-            .first();
+          .select()
+          .from('users')
+          .where('id', user._id)
+          .first()
 
       if (exist && (exist._id || exist.id)) {
-        if (user.email !== exist.email)
-          throw "Acesso não autorizado, seu e-mail de acesso foi alterado.";
+        if (user.email !== exist.email) {
+          throw {
+            name: 'changedEmail',
+            description: 'Acesso não autorizado, seu e-mail de acesso foi alterado.'
+          }
+        }
 
         if (payload.exp - Math.floor(Date.now() / 1000) < 60 * 60 * 24 * 2) {
-          payload.exp = 60 * 60 * 24 * 10;
-          token = await jwt.encode(payload, authSecret);
+          payload.exp = 60 * 60 * 24 * 10
+          token = jwt.encode(payload, secret)
         }
 
         if (exist.id) {
-          exist._id = exist.id;
-          delete exist.id;
+          exist._id = exist.id
+          delete exist.id
         }
 
-        exist.password = null;
+        exist.password = null
 
         return res.json({
           token,
           user: exist
-        });
+        })
       } else {
-        throw "Acesso não autorizado";
+        throw {
+          name: 'unauthorized',
+          description: 'Acesso não autorizado'
+        }
       }
     } catch (error) {
-      error = await validateTokenManagement(error);
-      return res.status(error.code).send(error.msg);
+      const stack = await authError(error)
+      return res.status(stack.code).send(stack)
     }
-  };
+  }
 
-  return { signIn, validateToken };
-};
+  return { signIn, validateToken }
+}
