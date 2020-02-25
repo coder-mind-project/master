e7e057dd24bfd2ff406364e4caa9e2c805b6e9df
Allan Wanderley <awallan259@gmail.com>
2020-02-25 17:18:44 -0300

    Removed comments in pt-br language and added spec docs

--- config/serialization/multer.js
+++ config/serialization/multer.js
@@ -1,28 +1,24 @@
 const multer = require('multer')
 
 /**
- *  Configuração para o disk storage do multer
- *  É necessário para persistir os arquivos enviados para a aplicação
- *  
- *  Obs: Até então o disk Storage esta configurado para somente persistir imagens
+ * @module multer
+ * @description Data store configuration.
+ * @returns {Function} A multer middleware.
  */
-
-module.exports = (multer(
-    {
-        storage: multer.diskStorage({
-            destination: (req, file, cb) => {
-                cb(null, './public/imgs/')
-            },
-            filename: (req, file, cb) => {
-                const fileName = `${Date.now()}`
-                cb(null, `${fileName}`)
-            }
-        }),
-        fileFilter: (req, file, cb) => {
-            const fileTypes = ['image/png', 'image/jpg', 'image/jpeg']
-            const isAccepted = fileTypes.find(currentFormat => currentFormat == file.mimetype)
-            return cb(null, isAccepted)
-        },
-        limits: 10000
+module.exports = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './public/imgs/')
+    },
+    filename: (req, file, cb) => {
+      const fileName = `${Date.now()}`
+      cb(null, `${fileName}`)
     }
-))
+  }),
+  fileFilter: (req, file, cb) => {
+    const fileTypes = ['image/png', 'image/jpg', 'image/jpeg']
+    const isAccepted = fileTypes.find(currentFormat => currentFormat === file.mimetype)
+    return cb(null, isAccepted)
+  },
+  limits: 10000
+})
