2e9183fe59efc70d55bfced9bf9342cd9f527652
Allan Wanderley <awallan259@gmail.com>
2020-02-25 17:10:19 -0300

    Changed location of spec doc

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
+const { dbLocal, dbProduction, dbDevelopment } = require('./.env')
+
 /**
  * @module knexfile
  * @description MySQL connection settings
  */
-const { dbLocal, dbProduction, dbDevelopment } = require('./.env')
-
 module.exports = {
   development: { ...dbDevelopment.mysql },
   production: { ...dbProduction.mysql },
