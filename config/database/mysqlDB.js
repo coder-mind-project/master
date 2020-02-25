bff974b1e3160a7688f5a828bcbd136faa82b345
Allan Wanderley <awallan259@gmail.com>
2020-02-25 18:09:20 -0300

    Added spec doc

--- config/database/mysqlDB.js
+++ config/database/mysqlDB.js
@@ -1,6 +1,11 @@
 const knex = require('knex')
 const config = require('../../knexfile.js')
 
+/**
+ * @module Knex
+ * @description Provide Mysql Connection into app consign Object.
+ * @param {Object} app - A app Object provided by consign.
+ */
 module.exports = app => {
   const { local, development, production } = config
   const connection = knex(development)
