d875f5811a909217ca991375ce3701f5ef65899d
Allan Wanderley <awallan259@gmail.com>
2020-02-25 03:26:32 -0300

    Added spec docs

--- config/cron.js
+++ config/cron.js
@@ -1,27 +1,57 @@
 const schedule = require('node-schedule')
 
+/**
+ * @function
+ * @module cron
+ * @description Manage cron jobs for aplication.
+ * @param {Object} app - A app Object provided by consign.
+ */
 module.exports = app => {
+  const { viewsJob } = app.api.articles.views
+  const { commentsJob } = app.api.articles.comments
+  const { likesJob } = app.api.articles.likes
 
-    const { viewsJob } = app.api.articles.views
-    const { commentsJob } = app.api.articles.comments
-    const { likesJob } = app.api.articles.likes 
+  const { validateFirstLoginTime, writeRemovedUsers } = app.api.users.users
 
-    const { validateFirstLoginTime, writeRemovedUsers } = app.api.users.users
+  /**
+   * @function
+   * @description Run a syncronization job for migrate current views by autor and global to MySQL database.
+   * @param {String} - A schedule config, run in every 01:00:00 AM.
+   * @param {Function} - A callback to run job.
+   */
+  schedule.scheduleJob('00 01 * * *', () => {
+    viewsJob()
+  })
 
-    schedule.scheduleJob('00 01 * * *', async () => {
-        viewsJob()
-    })
+  /**
+   * @function
+   * @description Run a syncronization job for migrate current comments by autor and global to MySQL database.
+   * @param {String} - A schedule config, run in every 02:00:00 AM.
+   * @param {Function} - A callback to run job.
+   */
+  schedule.scheduleJob('00 02 * * *', () => {
+    commentsJob()
+  })
 
-    schedule.scheduleJob('00 02 * * *', async () => {
-        commentsJob()
-    })
+  /**
+   * @function
+   * @description Run a syncronization job for migrate current likes by autor and global to MySQL database.
+   * @param {String} - A schedule config, run in every 03:00:00 AM.
+   * @param {Function} - A callback to run job.
+   */
+  schedule.scheduleJob('00 03 * * *', () => {
+    likesJob()
+  })
 
-    schedule.scheduleJob('00 03 * * *', async () => {
-        likesJob()
-    })
-
-    schedule.scheduleJob('00 04 * * *', async () => {
-        const resultSet = await validateFirstLoginTime()
-        writeRemovedUsers(resultSet)
-    })
+  /**
+   * @function
+   * @description Run a syncronization job for get inactive users
+   * (that not login in first time on seven days) and write these removed users.
+   * @param {String} - A schedule config, run in every 04:00:00 AM.
+   * @param {Function} - A callback to run job.
+   */
+  schedule.scheduleJob('00 04 * * *', async () => {
+    const resultSet = await validateFirstLoginTime()
+    writeRemovedUsers(resultSet)
+  })
 }
