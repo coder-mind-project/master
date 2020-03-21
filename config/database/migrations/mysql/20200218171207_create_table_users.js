2eda588f7ee50d9fea92f403f806649eb2c291b0
Allan Wanderley <awallan259@gmail.com>
2020-03-21 17:35:09 -0300

    Added '// prettier-ignore' comment in property definitions

--- config/database/migrations/mysql/20200218171207_create_table_users.js
+++ config/database/migrations/mysql/20200218171207_create_table_users.js
@@ -2,14 +2,10 @@ exports.up = function(knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id')
     table.string('name').notNullable()
-    table
-      .string('email')
-      .notNullable()
-      .unique()
-    table
-      .string('username')
-      .notNullable()
-      .unique()
+    // prettier-ignore
+    table.string('email').notNullable().unique()
+    // prettier-ignore
+    table.string('username').notNullable().unique()
     table.string('tagAdmin').notNullable()
     table.string('password').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now())
