0b42b7a202d3de86ec10380b4b50294d470db088
Allan Wanderley <awallan259@gmail.com>
2020-03-25 23:27:00 -0300

    Changed newAccount puretext template location

--- api/users/emails.js
+++ api/users/emails.js
@@ -1,6 +1,6 @@
 const userChangedTxtMsg = require('../../mailer-templates/mail-text-msg/userChanged')
 const userChangedToOldMailTxtMsg = require('../../mailer-templates/mail-text-msg/userChangedToOldMail')
-const newAccountTxtMsg = require('../../mailer-templates/mail-text-msg/newAccount')
+const newAccountTxtMsg = require('../../assets/emails/puretext/newAccount')
 const emailChangedMyAccountTxtMsg = require('../../mailer-templates/mail-text-msg/emailChangedMyAccount')
 
 const { panel } = require('../../.env')
