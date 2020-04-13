bb9ff51068e9b67c189b263bcb6e5383b30cdf08
Allan Wanderley <awallan259@gmail.com>
2020-04-13 17:47:41 -0300

    Created comment settings table

---
+++ config/database/migrations/mysql/20200413145116_create_table_comments_settings.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('comment_settings', table => {
+    table.increments('id')
+    table.string('userId').notNullable().unique()
+    table.enu('type', ['all', 'only-readed', 'not-readed']).defaultTo('all').notNullable()
+    table.enu('order', ['desc', 'asc']).defaultTo('desc').notNullable()
+    table.integer('limit').defaultTo(6).notNullable()
+    table.boolean('notify').defaultTo(false).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+    table.engine('InnoDB')
+    table.charset('utf8mb4')
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('comment_settings')
+}
