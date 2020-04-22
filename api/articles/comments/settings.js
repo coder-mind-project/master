57b56d40f97c4f1b6cc2c01ebbf588990793333b
Allan Wanderley <awallan259@gmail.com>
2020-04-21 21:21:16 -0300

    Added 2 new fields into 'comment settings' module and fixed 'enum' fields validation

--- api/articles/comments/settings.js
+++ api/articles/comments/settings.js
@@ -35,6 +35,56 @@ module.exports = app => {
 
   /**
    * @function
+   * @description Verify all `enum` fields in `comment_settings` table
+   * @private
+   * @param {Object} settings Comment settings object representation
+   */
+  const verifyEnumFields = settings => {
+    if (
+      settings.answersType &&
+      settings.answersType !== 'all' &&
+      settings.answersType !== 'not-readed' &&
+      settings.answersType !== 'only-readed' &&
+      settings.answersType !== 'disabled' &&
+      settings.answersType !== 'enabled'
+    ) {
+      throw {
+        name: 'answersType',
+        description: 'Tipo de respostas inválido'
+      }
+    }
+
+    if (
+      settings.type &&
+      settings.type !== 'all' &&
+      settings.type !== 'not-readed' &&
+      settings.type !== 'only-readed' &&
+      settings.type !== 'disabled' &&
+      settings.type !== 'enabled'
+    ) {
+      throw {
+        name: 'type',
+        description: 'Tipo de comentários inválido'
+      }
+    }
+
+    if (settings.answersOrder && settings.answersOrder !== 'desc' && settings.answersOrder !== 'asc') {
+      throw {
+        name: 'answersOrder',
+        description: 'Ordem de respostas inválida'
+      }
+    }
+
+    if (settings.order && settings.order !== 'desc' && settings.order !== 'asc') {
+      throw {
+        name: 'order',
+        description: 'Ordem de comentários inválido'
+      }
+    }
+  }
+
+  /**
+   * @function
    * @description Get current comment settings by user Identifier
    *
    * @param {Object} req - Request object provided by Express.js
@@ -58,7 +108,26 @@ module.exports = app => {
 
       await verifyUser(user)
 
-      const settings = await app.knex.select('userId', 'type', 'order', 'limit', 'notify').from('comment_settings').where('userId', user._id).first()
+      const settings = await app.knex
+        .select(
+          'userId',
+          'type',
+          'order',
+          'limit',
+          'notify',
+          'answers_type as answersType',
+          'answers_order as answersOrder'
+        )
+        .from('comment_settings')
+        .where('userId', user._id)
+        .first()
+
+      if (!settings) {
+        throw {
+          name: 'id',
+          description: 'Este usuário não possui configurações de comentário definida'
+        }
+      }
 
       settings.notify = Boolean(settings.notify)
 
@@ -82,7 +151,9 @@ module.exports = app => {
    */
   const save = async (req, res) => {
     try {
-      const { notify, limit, order, type } = req.body
+      verifyEnumFields(req.body)
+
+      const { notify, limit, order, type, answersOrder, answersType } = req.body
       const { user } = req.user
 
       await verifyUser(user)
@@ -90,20 +161,44 @@ module.exports = app => {
       const currentSettings = await app.knex.select().from('comment_settings').where('userId', user._id).first()
 
       if (!currentSettings) {
-        await app.knex.insert({ notify, limit, order, type, userId: user._id }).into('comment_settings')
+        await app.knex
+          .insert({
+            notify,
+            limit,
+            order,
+            type,
+            userId: user._id,
+            answers_order: answersOrder,
+            answers_type: answersType
+          })
+          .into('comment_settings')
       } else {
         const updatedSettings = {
           limit: limit || currentSettings.limit,
           notify: notify !== undefined && notify !== null ? notify : currentSettings.limit,
           order: order || currentSettings.order,
           type: type || currentSettings.type,
+          answers_order: answersOrder || currentSettings.answers_order,
+          answers_type: answersType || currentSettings.answers_type,
           updated_at: app.knex.fn.now()
         }
 
         await app.knex('comment_settings').where('userId', user._id).update(updatedSettings)
       }
 
-      const settings = await app.knex.select('userId', 'type', 'order', 'limit', 'notify').from('comment_settings').where('userId', user._id).first()
+      const settings = await app.knex
+        .select(
+          'userId',
+          'type',
+          'order',
+          'limit',
+          'notify',
+          'answers_type as answersType',
+          'answers_order as answersOrder'
+        )
+        .from('comment_settings')
+        .where('userId', user._id)
+        .first()
 
       settings.notify = Boolean(settings.notify)
 
