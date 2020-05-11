e87b02e62219830d59469bb0ce657f6874083f36
Allan Wanderley <awallan259@gmail.com>
2020-05-11 13:36:29 -0300

    Refactor: Updated article view Schema

--- config/database/schemas/View.js
+++ config/database/schemas/View.js
@@ -5,13 +5,20 @@ const validator = require('mongoose-unique-validator')
  * @description The View (Article view) Schema
  * @type {mongoose.Schema}
  */
-const view = new mongoose.Schema({
-  _id: { type: mongoose.Schema.ObjectId, auto: true },
-  reader: String,
-  startRead: Date,
-  article: Object,
-  viewsQuantity: { type: Number, default: 1 }
-})
+const view = new mongoose.Schema(
+  {
+    _id: { type: mongoose.Schema.ObjectId, auto: true },
+    reader: { type: String, required: true },
+    articleId: { type: mongoose.Schema.ObjectId, required: true },
+    accessCount: { type: Number, default: 1, required: true, min: 1 }
+  },
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt'
+    }
+  }
+)
 
 view.plugin(validator)
 
