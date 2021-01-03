93e96148960a61ad2692d430a4ec9ca7346886de
Allan Wanerley Alves <awallan259@gmail.com>
2021-01-03 12:48:20 -0300

    chore AB#15: Removed not used vars and defined fixed env variables

--- config/database/mysqlDB.js
+++ config/database/mysqlDB.js
@@ -1,7 +1,7 @@
 const knex = require('knex')
 const config = require('../../knexfile.js')
 
-const logger = (message) => {
+const logger = message => {
   // eslint-disable-next-line no-console
   console.log(`Database settings - Migrations - ${new Date()} - ${message}`)
 }
@@ -12,23 +12,22 @@ const logger = (message) => {
  * @param {Object} app - A app Object provided by consign.
  */
 module.exports = app => {
-  const { local, development, production } = config
-  const connection = knex(development)
+  const { production } = config
+  const connection = knex(production)
   app.knex = connection
   migrate(connection)
 }
 
-const migrate = async (connection) => {
+const migrate = async connection => {
   let migrationsExecuted = 0
-  await connection.migrate.latest()
-    .then((action) => {
+  await connection.migrate
+    .latest()
+    .then(action => {
       const migrationsToExecute = action[1]
       migrationsToExecute.forEach(migration => {
         logger(`Running ${migration}...`)
         migrationsExecuted++
       })
     })
-    .then(
-      () => logger(`Database updated. - ${migrationsExecuted} migrations executed.`)
-    )
+    .then(() => logger(`Database updated. - ${migrationsExecuted} migrations executed.`))
 }
