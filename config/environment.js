8bec1b7072e9e5de2f8a496363ae90727c2b2372
Allan Wanerley Alves <awallan259@gmail.com>
2021-01-03 12:55:11 -0300

    chore AB#15: Cleanup exceeded variables

--- config/environment.js
+++ config/environment.js
@@ -17,9 +17,7 @@ const aws = {
 }
 
 const mongo = {
-  production: process.env.MONGOPRODUCTION,
-  develop: process.env.MONGODEVELOP,
-  local: process.env.MONGOLOCAL
+  production: process.env.MONGOPRODUCTION
 }
 
 const mysql = {
@@ -29,20 +27,6 @@ const mysql = {
     password: process.env.MYSQLPASSPRODUCTION,
     dbname: process.env.MYSQLDBNAMEPRODUCTION,
     port: process.env.MYSQLPORTPRODUCTION
-  },
-  develop: {
-    url: process.env.MYSQLDEVELOP,
-    user: process.env.MYSQLUSERDEVELOP,
-    password: process.env.MYSQLPASSDEVELOP,
-    dbname: process.env.MYSQLDBNAMEDEVELOP,
-    port: process.env.MYSQLPORTDEVELOP
-  },
-  local: {
-    url: process.env.MYSQLLOCAL,
-    user: process.env.MYSQLUSERLOCAL,
-    password: process.env.MYSQLPASSLOCAL,
-    dbname: process.env.MYSQLDBNAMELOCAL,
-    port: process.env.MYSQLPORTLOCAL
   }
 }
 
@@ -111,9 +95,10 @@ const rootUser = {
  */
 module.exports = {
   aws,
-  dbProduction: { url: mongo.production },
-  dbLocal: {
-    mongo: { url: mongo.local },
+  dbProduction: {
+    mongo: {
+      url: mongo.production
+    },
     mysql: {
       client: 'mysql',
       connection: {
@@ -129,34 +114,7 @@ module.exports = {
         max: 5,
         afterCreate: function (conn, done) {
           // eslint-disable-next-line no-console
-          console.log(`Mysql Connection Opened at ${new Date()}`)
-          done(null, conn)
-        }
-      },
-      acquireConnectionTimeout: 30000,
-      migrations: {
-        directory: './config/database/migrations/mysql'
-      }
-    }
-  },
-  dbDevelopment: {
-    mongo: { url: mongo.develop },
-    mysql: {
-      client: 'mysql',
-      connection: {
-        host: mysql.develop.url,
-        user: mysql.develop.user,
-        port: mysql.develop.port,
-        password: mysql.develop.password,
-        database: mysql.develop.dbname,
-        dateStrings: true
-      },
-      pool: {
-        min: 0,
-        max: 5,
-        afterCreate: function (conn, done) {
-          // eslint-disable-next-line no-console
-          console.log(`Mysql Connection Opened at ${new Date()}`)
+          console.log(`Mysql Connection Opened at ${new Date().toLocaleString()}`)
           done(null, conn)
         }
       },
